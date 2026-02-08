8d86d07a5c501f5096f8eca9ef26390746cdd700
wisted13 <rifatesov0@gmail.com>
2026-02-08 23:52:25 +0500

    Light v0.2

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
-version = "0.1"
+version = "0.2"
 versionType = "Alpha"
 
 print(f"Light {version} {versionType} - Created by wisted13\n")
 
-print("Loading modules...")
+print("Starting program...")
 
 try:
-    from server_mode import server_mode
-    from client_mode import client_mode
-
-    print("Modules loaded successfully!")
-except ImportError:
-    print("Error loading modules.")
-    exit()
+    print("Checking username...")
+    with open("username.txt", "r") as f:
+        username = f.read()
+except FileNotFoundError:
+    print("File not found, creating...")
+    username = None
 except Exception as e:
     print(f"Error: {e}")
     exit()
+    
+if username == None:
+    username = input("Enter your username: ")
+    with open("username.txt","a") as f:
+        f.write(username)
+else:
+    pass
 
 try:
     mode = int(input("\nEnter program mode (1 for Server, 2 for Client): "))
     
     if mode == 1:
-        server_mode()
+        print("\nLoading module...")
+        from server_mode import server_mode
+        print("Module successfully loaded.\n")
+        server_mode(username)
         exit()
     elif mode == 2:
-        client_mode()
+        print("\nLoading module...")
+        from client_mode import client_mode
+        print("Module successfully loaded.\n")
+        client_mode(username)
         exit()
     else:
-        print("Unknown mode.")
+        print("\nUnknown mode.")
         exit()
+except ImportError:
+    print("Error loading modules.")
+    exit()
 except ValueError:
-    print("Invalid input. Please enter a number.")
+    print("\nInvalid input. Please enter a number.")
     exit()
 except Exception as e:
-    print(f"Error: {e}")
+    print(f"\nError: {e}")
     exit()
